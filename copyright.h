989a6b4e6a76b8443fd242981f26de00cd9ced68
Vpredictor <vpredictor5@gmail.com>
2021-05-07 13:45:13 +0800

    add all files

---
+++ copyright.h
@@ -0,0 +1,14 @@
+#ifndef COPYRIGHT_H
+#define COPYRIGHT_H
+#ifdef __cplusplus
+	extern "C" {
+#endif
+
+/* Embed a copyright string */
+static volatile char __copyright__[] = "Copyright 1995-2013 Graeme W. Gill";
+
+#ifdef __cplusplus
+	}
+#endif
+
+#endif
---
+++ jversion.h
@@ -0,0 +1,16 @@
+/*
+ * jversion.h
+ *
+ * Copyright (C) 1991-2012, Thomas G. Lane, Guido Vollbeding.
+ * This file is part of the Independent JPEG Group's software.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
+ * This file contains software version identification.
+ */
+#ifndef JVERSION_H
+#define JVERSION_H
+#define JVERSION	"8d  15-Jan-2012"
+
+#define JCOPYRIGHT	"Copyright (C) 2012, Thomas G. Lane, Guido Vollbeding"
+
+#endif
